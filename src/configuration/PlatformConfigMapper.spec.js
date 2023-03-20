0a9438928995a6e256dd98fdd5a69da867727969
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2023-03-20 13:07:31 +0530

    [Japneet | Procheta] Add. csv file parsed to json result

--- src/configuration/PlatformConfigMapper.spec.js
+++ src/configuration/PlatformConfigMapper.spec.js
@@ -2,11 +2,18 @@ import {
   it, expect, describe,
 } from 'vitest';
 import PlatformConfigMapper from './PlatformConfigMapper';
+import sample from '../resource/test/sample.json';
 
 describe('PlatformConfigMapper', () => {
   it('should test if csv file is called ', async () => {
     const csvparser = new PlatformConfigMapper('src/resource/test/sample.csv');
     const csvData = await csvparser.parseCSV();
-    expect(csvData.length).toBe(3);
+    expect(csvData).toHaveLength(3);
+  });
+
+  it('should test if csv file has been parsed to json result ', async () => {
+    const csvparser = new PlatformConfigMapper('src/resource/test/sample.csv');
+    const csvData = await csvparser.parseCSV();
+    expect(csvData).toStrictEqual(sample);
   });
 });
