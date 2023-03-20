f8a327741e93c2fa77867ac427ff6d0d21ab423e
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2023-03-20 15:19:34 +0530

    [Japneet | Procheta] Add. PlatformMapper test

---
+++ src/convertor/OndcMapper.spec.js
@@ -0,0 +1,11 @@
+import {
+  it, expect, describe,
+} from 'vitest';
+
+describe('OndcMapper', () => {
+  it('should test if ondc id is mapped with platform id ', async () => {
+    // const ondcMapper = new OndcMapper('src/resource/test/sample.csv');
+    // const csvData = await ondcMapper.parseCSV();
+    expect('one').toBe('one');
+  });
+});
