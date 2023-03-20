e216a23c09e7f37cb7004db71df3f6f0fe04ce50
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2023-03-20 12:18:33 +0530

    [Japneet | Procheta] Modify. sample csv

--- src/configuration/PlatformConfigMapper.spec.js
+++ src/configuration/PlatformConfigMapper.spec.js
@@ -5,7 +5,7 @@ import PlatformConfigMapper from './PlatformConfigMapper';
 
 describe('PlatformConfigMapper', () => {
   it('should test if csv file is called ', async () => {
-    const csvparser = new PlatformConfigMapper('src/resource/test/item_mapping_config.csv');
+    const csvparser = new PlatformConfigMapper('src/resource/test/sample.csv');
     const csvData = await csvparser.parseCSV();
     expect(csvData.length).toBe(3);
   });
