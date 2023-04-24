0168bc26490c89e60873661b18f90358e5e55c13
shashank.sharma <Shashank_A_Sharma@gap.com>
2023-04-24 23:20:25 +0530

    update Name for base port

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Adapter');
 });
 app.post('/search', SearchController.search);
 app.listen(port, () => {
