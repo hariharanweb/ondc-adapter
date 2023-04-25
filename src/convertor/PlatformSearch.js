8edb4e5583554efcd545c1cf82fd96de9860ef08
shashank.sharma <Shashank_A_Sharma@gap.com>
2023-04-25 18:00:18 +0530

    category configuration

--- src/convertor/PlatformSearch.js
+++ src/convertor/PlatformSearch.js
@@ -1,13 +1,14 @@
 import LoggingService from '../utility/LoggingService.js';
 import OndcSearchExtractor from './OndcSearchExtractor.js';
 import ApiCaller from '../utility/ApiCaller.js';
+import PlatformConfigMapper from '../configuration/PlatformConfigMapper.js';
 
 const logger = LoggingService.getLogger('PlatformSearch');
 
 export default class PlatformSearch {
   static async getPlatformResponse(ondcRequest) {
     const ondcSearchJson = OndcSearchExtractor.extract(ondcRequest);
-    const platformRequest = PlatformSearch.generateRequest(ondcSearchJson);
+    const platformRequest = await PlatformSearch.generateRequest(ondcSearchJson);
     let platformResponse = await ApiCaller.makeHttpRequest(platformRequest);
     // Update it as array even when response is in Object format
     platformResponse = JSON.parse(platformResponse);
@@ -17,7 +18,7 @@ export default class PlatformSearch {
     return platformResponse;
   }
 
-  static generateRequest(inputObject) {
+  static async generateRequest(inputObject) {
     logger.debug(`Executing generateRequest ${inputObject}`);
     let urlString = '';
     if (inputObject.productName === '' && inputObject.productId === '' && inputObject.categoryId === '' && inputObject.categoryName === '') {
@@ -27,7 +28,11 @@ export default class PlatformSearch {
     } if (inputObject.productId !== '') {
       urlString = `${process.env.PLATFORM_BASE_URL}/${inputObject.productId}${process.env.PLATFORM_AUTH_QUERY_PARAM}`;
     } if (inputObject.categoryId !== '') {
-      urlString = `${process.env.PLATFORM_BASE_URL}${process.env.PLATFORM_AUTH_QUERY_PARAM}&category=${inputObject.categoryId}`;
+      const platformConfigMapper = new PlatformConfigMapper('src/resource/category_mapping_config.csv');
+      const categoryMapper = await platformConfigMapper.parseCSV();
+      const platformCategoryId = categoryMapper
+        .filter((x) => x.ondc_category === inputObject.categoryId)[0].platform_category;
+      urlString = `${process.env.PLATFORM_BASE_URL}${process.env.PLATFORM_AUTH_QUERY_PARAM}&category=${platformCategoryId}`;
     } if (inputObject.categoryName !== '') {
       urlString = `${process.env.PLATFORM_BASE_URL}/categories${process.env.PLATFORM_AUTH_QUERY_PARAM}&search=${inputObject.categoryName}`;
     }
